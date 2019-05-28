fd9fa72e4a21c08ed1725d8a620ac7e524f098f6
abdul-ashad15 <abdul.ashad1@gmail.com>
2019-05-29 01:27:30 +0530

    Test Project Added

---
+++ EmployeeMVC/EmployeeService.Test/EmployeeTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeService;
+using EmployeeService.Models;
+
+namespace EmployeeService.Test
+{
+    [TestClass]
+    public class EmployeeTest
+    {
+        [TestMethod]
+        public void GetEmployeeFirstName()
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = employeeContext.employees.FirstOrDefault(emp => emp.EmpId == 1001);
+            Assert.AreEqual("Abdul1", employee.FirstName);
+        }
+    }
+}
---
+++ EmployeeMVC/EmployeeService.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeService.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeService.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b675dbb7-b824-4501-b979-7a4c9d3f4bbb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

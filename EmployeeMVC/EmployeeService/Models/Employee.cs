1bb5e58df28baa88f45d2f725aa25b5340503421
abdul-ashad15 <abdul.ashad1@gmail.com>
2019-05-28 18:31:37 +0530

    Remove Salary Property

--- EmployeeMVC/EmployeeService/Models/Employee.cs
+++ EmployeeMVC/EmployeeService/Models/Employee.cs
@@ -14,6 +14,5 @@ namespace EmployeeService.Models
         public int? EmpId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        //public double Salary { get; set; }
     }
 }
